242aaeba1846c963160a0d2a5e43c35626422cfc
Simon Jamain <jamain.simon@gmail.com>
2026-07-19 02:22:32 +0200

    fix dummy photo path

--- src/extensions/dummy/dummyCameraNode.ts
+++ src/extensions/dummy/dummyCameraNode.ts
@@ -1,9 +1,10 @@
+const dummyCameraPictureUrl = new URL("./dummyCameraPicture.jpg", import.meta.url).href;
+
 export const dummyCameraNode = {
     name: "dummyCameraNode",
     async capture(): Promise<ImageBitmap[]> {
         console.debug("dummyCameraNode: capture called");
-        // Use the dummy jpg image as a placeholder for the captured image
-        const response = await fetch("/dummyCameraPicture.jpg");
+        const response = await fetch(dummyCameraPictureUrl);
         const blob = await response.blob();
         const imageBitmap = await createImageBitmap(blob);
         return [imageBitmap];
