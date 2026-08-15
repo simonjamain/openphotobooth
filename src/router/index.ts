d8115a0d58a6ffc88419e7c6cd70585d8f1e9c7b
simonjamain <jamain.simon@gmail.com>
2026-08-15 12:28:12 +0000

    fix pages deploy

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import boothView from '@/views/boothView.vue';
 import flowConfigView from '@/views/flowConfigView.vue';
 import flowsView from '@/views/flowsView.vue';
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/booth',
